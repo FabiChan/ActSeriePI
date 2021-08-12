7de0c2ca72d08202cfe382575a20c25eca4d4cc7
Fabi <66051462+FabiChan@users.noreply.github.com>
2021-08-11 21:50:28 -0500

    Código Método main()
    Código hecho por Leslie, para hacer el main

---
+++ Serie.java
@@ -0,0 +1,26 @@
+//Equipo alfa buena maravilla onda dinamita escuadrón lobo
+//Objetivo del programa: calcular la suma de la serie
+import java.io.*;
+import java.util.*;
+import java.text.*;
+class Serie {
+   public static void main(String arg[]) {
+      double x = Serie.pedirX();
+      //Si x = 0, automáticamente la suma de la serie es 1
+      if(x!=0) {
+         double suma = 0;
+         double termino = 1, fact = 1;
+         byte n = 1;
+         do {
+            suma+=termino;
+            fact = Serie.calcularFactorial(n,fact);
+            termino = Serie.sacarTermino(x,n,fact);
+            n++;
+         }while(termino > (Math.pow(10,-8)));
+      else
+         byte suma = 1;
+      Serie.imprimir(suma);
+   }
+}
+  
+  
