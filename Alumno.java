9487d3950403484d19a932bfc32a7948716ac883
Fátima Morales <70866714+fatimamv@users.noreply.github.com>
2021-09-03 09:22:05 -0500

    Update Alumno.java

--- Alumno.java
+++ Alumno.java
@@ -55,16 +55,16 @@ public class Alumno {
          Scanner s = new Scanner(System.in);
          int matricula;
          do{
-         System.out.println("Escriba su matricula");
-         matricula = s.nextInt();
+         	System.out.println("Escriba su matricula");
+         	matricula = s.nextInt();
          }while(matricula<0);
          int cantCreditos;
          do{
          	System.out.println("Escriba la cantidad de creditos que cursara");
          	cantCreditos = s.nextInt();
          }while(cantCreditos<0);
-         	Alumno A1 = new Alumno(matricula, cantCreditos);
-         	System.out.println(A1);
-         }
+         Alumno A1 = new Alumno(matricula, cantCreditos);
+         System.out.println(A1);
+      }
          
-    }
+   }
