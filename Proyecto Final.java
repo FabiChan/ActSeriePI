1d637906519584dd90e5fc17e520f0ad0d85345b
mariafernanda-vazquez <83107598+mariafernanda-vazquez@users.noreply.github.com>
2021-11-11 02:37:45 -0600

    Update Proyecto Final.java

--- Proyecto Final.java
+++ Proyecto Final.java
@@ -1,8 +1,6 @@
 import java.io.*;
 import java.util.*;
-
-
-//Clase padre Producto 
+//clase padre Producto 
 class Producto{
    protected int codigoProd;
    protected String nomProd;
@@ -183,7 +181,23 @@ class Fecha {
    }
 }
 
-//Clase para el nombre
+//clase qu guarda las cuentas del sistema usuario-contraseña
+class UsuarioContrasena {
+   HashMap<String,String> accounts = new HashMap<String,String>();
+   
+   //metodo constructor que crea el "diccionario" de usuarios con sus contraseñas
+   UsuarioContrasena() {
+      accounts.put("Arturo","abc123*");
+      accounts.put("Laura","p4t1t0");
+      accounts.put("Juan","PASSWORD");
+      accounts.put("Maria","qwerty");
+   }
+   
+   protected HashMap getAccounts() {
+      return accounts;
+   }
+}
+
 class Nombre
 {
    private String primerNombre;
@@ -233,96 +247,123 @@ class Nombre
    }
 }
 
-//Clase Venta
+
+class Empleado{
+    private Nombre nomEmpleado;
+    private Fecha cumple;
+    public Empleado(Nombre nomEmpleado, Fecha cumple){
+        setNomEmpleado(nomEmpleado);
+        setCumple(cumple);
+    }
+    public void setNomEmpleado(Nombre nomEmpleado) {
+        this.nomEmpleado = nomEmpleado;
+    }
+    public void setCumple(Fecha cumple) {
+        this.cumple = cumple;
+    }
+    public Nombre getNomEmpleado() {
+        return nomEmpleado;
+    }
+    public Fecha getCumple() {
+        return cumple;
+    }
+    public String toString() {
+
+        return "Empleado: "+getNomEmpleado()+" " +getCumple();
+    }
+}
+
 class Venta{
-   private int codigoVenta;
-   private Empleado empleado;
-   private Producto[] compras;
-   private Fecha fechaVenta;
-   private double descuento;
-   
-//Metodo constructor
-   public Venta(int codigoVenta, Empleado empleado, Producto[] compras, Fecha fechaVenta, double descuento){
-      setCodigoVenta(codigoVenta);
-      setEmpleado(empleado);
-      setCompras(compras);
-      setFechaVenta(fechaVenta);
-      setDescuento(descuento);
-   }
-   
-   public void setCodigoVenta(int codigoVenta)
-   {
-      this.codigoVenta=codigoVenta;
-   }
+    private int codigoVenta;
+    private Empleado empleado;
+    private Producto compras[];
+    private double descuento;
+    public Venta(int codigoVenta, Empleado empleado, Producto compras[], double descuento){
+        setCodigoVenta(codigoVenta);
+        setEmpleado(empleado);
+        setCompras(compras);
+        setDescuento(descuento);
+    }
+    public void setCodigoVenta(int codigoVenta) {
+        this.codigoVenta = codigoVenta;
+    }
+    public void setEmpleado(Empleado empleado) {
+        this.empleado = empleado;
+    }
+    public void setCompras(Producto[] compras) {
+        this.compras = compras;
+    }
+    public void setDescuento(double descuento) {
+        this.descuento = descuento;
+    }
+    public int getCodigoVenta() {
+        return codigoVenta;
+    }
+    public Empleado getEmpleado() {
+        return empleado;
+    }
+    public Producto[] getCompras() {
+        return compras;
+    }
+    public double getDescuento() {
+        return descuento;
+    }
 
-   public void setEmpleado(Empleado empleado)
-   {
-      this.empleado=empleado;
-   }
+    public double calcularTotal(){
+        return calcularSubtotal()-calcularDescuento()+calcularIVA();
+    }
+    public double calcularSubtotal(){
+        double subtotal = 0;
+      for(int i=0; i<compras.length; i++) 
+         subtotal += compras[i].getCantidad() * compras[i].getPrecio();
+      return subtotal;
 
-   public void setCompras(Producto[] compras)
-   {
-      this.compras=compras;
-   }
-   
-   public void setFechaVenta(Fecha fechaVenta)
-   {
-      this.fechaVenta=fechaVenta;
-   }
-   
-   public void setDescuento(double descuento)
-   {
-      this.descuento=descuento;
-   }
+    }
+    public double calcularIVA(){
+        return (calcularSubtotal()-calcularDescuento())*0.16;
+    }
+    public double calcularDescuento(){
+        double subtotal = calcularSubtotal();
+    if (descuento != 0)  
+      return subtotal*descuento*0.01;
+    return descuento;
+    }
 
-   public int getCodigoVenta()
-   {
-      return codigoVenta;
-   }
 
-   public Empleado getEmpleado()
-   {
-      return empleado;
-   }
-
-   public Producto[] getCompras()
-   {
-      return compras;
-   }
-   
-   public Fecha getFechaVenta()
-   {
-      return fechaVenta;
-   }
-   
-   public double getDescuento()
-   {
-      return descuento;
-   }
-   
-   public String toString()
-   {
-      
-   }
-      
+    public String toString() {
+        //DecimalFormat d= new DecimalFormat("0.00");
+        String salida = "\n-------------------------------Venta-------------------------------\n" + "\n";
+        for (byte i=0;i<compras.length;i++) {
+          salida += "\n"+(Integer.toString(compras[i].getCodigoProd()))+(Integer.toString(compras[i].getCantidad()))+(compras[i].getNomProd())+((compras[i].getPrecio()))+"\n";
+         }
+        salida += "\n\n                                             Descuento:     "+((calcularDescuento()))+"\n                                             Subtotal:      "+((calcularSubtotal()))+"\n                                             IVA:           "+((calcularIVA()))+"\n                                             Total:         "+ ((calcularTotal())); 
+        return salida;             
+    }
 }
 
-//clase qu guarda las cuentas del sistema usuario-contraseña
-public class UsuarioContrasena {
-   HashMap<String,String> accounts = new HashMap<String,String>();
-   
-   //metodo constructor que crea el "diccionario" de usuarios con sus contraseñas
-   UsuarioContrasena() {
-      accounts.put("Arturo","abc123*");
-      accounts.put("Laura","p4t1t0");
-      accounts.put("Juan","PASSWORD");
-      accounts.put("Maria","qwerty");
-   }
-   
-   protected HashMap getAccounts() {
-      return accounts;
-   }
+class ReporteDiario{
+    private Venta ventasDia;
+    public ReporteDiario (Venta ventasDia){
+        setVentasDia(ventasDia);
+    }
+    public void setVentasDia(Venta ventasDia) {
+        this.ventasDia = ventasDia;
+    }
+    public Venta getVentasDia() {
+        return ventasDia;
+    }
+    public int calcularNumVentas(){
+        return 0;
+    }
+    public double calcularTotalIngresos(){
+        return 0;
+    }
+
+    public String toString() {
+        return "Reporte Diario: ";
+    }
 }
+
 
 
 //clase principal del sistema
@@ -333,6 +374,22 @@ class Principal {
       boolean acceso = p.darAcceso();
       if (acceso) {
          System.out.println("\n'Bienvenido!'");
+         Nombre nombre=new Nombre(p.validarString("Ingrese el nombre: "), p.validarString("Ingresa el apellido paterno: "), p.validarString("Ingresar el apellido materno: "));
+         Fecha fecha=new Fecha(p.validarInt("Ingresa el dia: "), p.validarInt("Ingresa el mes: "), p.validarInt("Ingresa el año: "));
+         Empleado empleado=new Empleado(nombre,fecha); 
+         Scanner s=new Scanner (System.in);
+         System.out.println("Ingrese cuantos productos desea comprar (max 5)");
+         int n = s.nextInt();
+         Producto compra[] = new Producto [n];
+         for (int i = 0; i<n; i++) {
+            compra[i] = new Producto(p.validarInt("Ingresa el código: "), p.validarString("Ingresa el nombre del producto: "), p.validarDouble("Ingresa el precio: "), p.validarString("Ingresa la descripcion: "), p.validarInt("Ingresa la cantidad: "));
+         }
+         Venta venta=new Venta(p.validarInt("Ingrese el código de venta:"), empleado, compra, p.validarDouble("Ingrese el descuento: "));
+         System.out.println(nombre);
+         System.out.println(fecha);
+         System.out.println(empleado);
+         System.out.println(venta);
+
       }
       else
          System.out.println("\n***Se supero el numero maximo de intentos***");
@@ -368,5 +425,49 @@ class Principal {
       }
       return acceso;
    }
+   public String validarString(String mensaje){
+    //Validar la cantidad máxima para que no salga mal en la salida
+    Scanner s=new Scanner(System.in);
+        System.out.println(mensaje);
+        return s.nextLine();
 }
+
+    public int validarInt(String mensaje){
+        int numEntero=0;
+        String numeroS="";
+        boolean verdadero=true;
+    do{
+        Scanner s=new Scanner (System.in);
+        System.out.println(mensaje);
+        numeroS=s.nextLine();
+        try{
+            numEntero=Integer.parseInt(numeroS);
+        }catch(NumberFormatException e){
+            verdadero=false;
+            System.out.println("Tiene que ser un número entero");
+        }
+        }while (verdadero==false);
+        return numEntero;
+    }
+
+    public double validarDouble(String mensaje){
+        double numDouble=0;
+        String numeroS="";
+        boolean verdadero=true;
+    do{
+            Scanner s=new Scanner(System.in);
+            System.out.println("Ingresa el precio: ");
+            numeroS=s.nextLine(); 
+            try {  
+            numDouble=Double.parseDouble(numeroS); 
+            }catch(NumberFormatException e){ 
+            verdadero=false;
+            System.out.println("Tiene que ser un numero"); 
+            } 
+        }while(verdadero==false);
+        return numDouble;
+    }
+}
+
+
 
